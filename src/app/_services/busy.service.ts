7be653bfd70087f02273d93dae95ecd3395229fe
kmryc <kmrychlik05@gmail.com>
2021-10-25 16:16:10 +0200

    End of Section 10

---
+++ src/app/_services/busy.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  busyRequestCount = 0;
+
+  constructor(private spinnerService: NgxSpinnerService) { }
+
+  busy() {
+    this.busyRequestCount++;
+    this.spinnerService.show(undefined, {
+      type: 'line-scale-party',
+      bdColor: 'rgba(120,120,120,0)',
+      color: '#0066ff'
+    })
+  }
+
+  idle() {
+    this.busyRequestCount--;
+    if (this.busyRequestCount <= 0) {
+      this.busyRequestCount = 0;
+      this.spinnerService.hide();
+    }
+  }
+}
